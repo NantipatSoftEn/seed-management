23e5571d5e822a39291802f43b6483efbbed63a5
DESKTOP-9STFKGG\keyma <keymasterviriya1150@gmail.com>
2024-08-23 02:40:55 +0700

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,22 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+const DEV_PORT = 2121;
 
-import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
-
-const DEV_PORT = 2121
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.CI
-    ? 'https://themesberg.github.io'
-    : `http://localhost:${DEV_PORT}`,
+  site: process.env.CI ? 'https://themesberg.github.io' : `http://localhost:${DEV_PORT}`,
   base: process.env.CI ? '/flowbite-astro-admin-dashboard' : undefined,
   output: 'server',
-
   /* Like Vercel, Netlify,… Mimicking for dev. server */
   // trailingSlash: 'always',
 
   server: {
     /* Dev. server only */
-    port: DEV_PORT,
+    port: DEV_PORT
   },
-
   integrations: [
-    //
-    sitemap(),
-    tailwind(),
-  ],
-})
+  //
+  sitemap(), tailwind()]
+});
