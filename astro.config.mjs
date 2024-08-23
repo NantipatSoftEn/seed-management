5be52a12e4dc4900d3f2eebb726917ce97e14c8e
DESKTOP-9STFKGG\keyma <keymasterviriya1150@gmail.com>
2024-08-23 17:07:18 +0700

    "Update astro.config.mjs and src/app/SideBar.astro with various changes to class names, href attributes, and script logic."

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ const DEV_PORT = 2121;
 // https://astro.build/config
 export default defineConfig({
   site: process.env.CI ? 'https://themesberg.github.io' : `http://localhost:${DEV_PORT}`,
-  // base: process.env.CI ? '/flowbite-astro-admin-dashboard' : undefined,
+  base: process.env.CI ? '/flowbite-astro-admin-dashboard' : undefined,
   output: 'server',
   /* Like Vercel, Netlify,… Mimicking for dev. server */
   // trailingSlash: 'always',
