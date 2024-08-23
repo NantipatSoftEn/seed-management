5804ae8f13f72d13172d7eed938988d8241d93a9
DESKTOP-9STFKGG\keyma <keymasterviriya1150@gmail.com>
2024-08-23 18:09:03 +0700

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ const DEV_PORT = 2121;
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.CI ? '' : `http://localhost:${DEV_PORT}`,
+  site: process.env.CI ? 'https://seed-management.vercel.app' : `http://localhost:${DEV_PORT}`,
   base: process.env.CI ? '/flowbite-astro-admin-dashboard' : undefined,
   output: 'server',
   /* Like Vercel, Netlify,… Mimicking for dev. server */
