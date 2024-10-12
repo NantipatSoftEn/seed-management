3fe9862024cc350a1def30b700e42257c0212c31
Nantipat <keymasterviriya1150@gmail.com>
2024-10-12 15:01:07 +0700

    "Updated Astro config to use React integration, added React dependencies, and modified SideBar and LambSettings components"

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,22 +3,24 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
 
+import react from '@astrojs/react';
+
 const DEV_PORT = 2121;
 
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://seed-management.vercel.app',
+  // site: 'https://seed-management.vercel.app',
+    site: 'http://localhost:2121',
   base: '',
   output: 'server',
   /* Like Vercel, Netlify,… Mimicking for dev. server */
   // trailingSlash: 'always',
-	adapter: vercel(),
+    adapter: vercel(),
   server: {
     /* Dev. server only */
     port: DEV_PORT
   },
-  integrations: [
-  //
-  sitemap(), tailwind()]
+  integrations: [//
+  sitemap(), tailwind(), react()]
 });
---
+++ src/lib/excel.ts
@@ -0,0 +1,4 @@
+import XLSX from 'xlsx';
+
+// อ่านไฟล์ Excel
+const workbook = XLSX.readFile('path_to_file.xlsx');
