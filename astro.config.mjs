137e8b1be06f7283e2f88c1a88984e2e0f5c7af9
Nantipat <keymasterviriya1150@gmail.com>
2024-10-13 19:33:31 +0700

    "Updated Astro config: swapped site URLs, uncommented Vercel URL and commented out localhost URL."

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,8 +10,8 @@ const DEV_PORT = 2121;
 
 // https://astro.build/config
 export default defineConfig({
-  // site: 'https://seed-management.vercel.app',
-    site: 'http://localhost:2121',
+  site: 'https://seed-management.vercel.app',
+  // site: 'http://localhost:2121',
   base: '',
   output: 'server',
   /* Like Vercel, Netlify,… Mimicking for dev. server */
