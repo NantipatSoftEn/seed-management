b44d3fa4b7b94ca650d7eb46e9b14f6927dc6139
DESKTOP-9STFKGG\keyma <keymasterviriya1150@gmail.com>
2024-12-29 02:57:50 +0700

    `Updated CrudLambs component with new table layout and removed unused code`

---
+++ src/components/HeaderLinkR.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+interface HeaderLinkProps {
+  href: string;
+  className?: string;
+  children: React.ReactNode;
+  [key: string]: any;
+}
+
+const HeaderLink: React.FC<HeaderLinkProps> = ({ href, className, ...props }) => {
+  const pathname = window.location.pathname;
+  const subpath = pathname.match(/[^\/]+/g);
+  const isActive = href === pathname || href === '/' + subpath?.[0];
+
+  return (
+    <div>
+      <a href={href} className={`${className} ${isActive ? 'active' : ''}`} {...props}>
+        {props.children}
+      </a>
+     
+    </div>
+  );
+};
+
+export default HeaderLink;
--- src/modules/CrudLambs.tsx
+++ src/modules/CrudLambs.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { asset, fetchData, url } from '../lib/data'
 import type { Lamb } from 'src/types/InfoLamb'
+import HeaderLink from '$components/HeaderLinkR'
+import { getPersoanlityColor } from '$constants/personalityType'
 
 interface CrudLambsProps {
   clazz: string
@@ -19,7 +21,7 @@ const CrudLambs = () => {
   }, [])
 
   return (
-    <div>
+    <div className="block overflow-hidden shadow">
       <div className="block items-center justify-between border-b border-gray-200 bg-white p-4 dark:border-gray-700 dark:bg-gray-800 sm:flex lg:mt-1.5">
         <div className="mb-1 w-full">
           <div className="mb-4">
@@ -188,9 +190,9 @@ const CrudLambs = () => {
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                   ></path>
                 </svg>
                 Add user
@@ -213,6 +215,215 @@ const CrudLambs = () => {
                 </svg>
                 Export
               </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col">
+        <div className="overflow-x-auto">
+          <div className="inline-block min-w-full align-middle">
+            <div className="overflow-hidden shadow">
+              <table className="min-w-full table-fixed divide-y divide-gray-200 dark:divide-gray-600">
+                <thead className="bg-gray-100 dark:bg-gray-700">
+                  <tr>
+                    <th scope="col" className="p-4">
+                      <div className="flex items-center">
+                        <input
+                          id="checkbox-all"
+                          aria-describedby="checkbox-1"
+                          type="checkbox"
+                          className="focus:ring-3 h-4 w-4 rounded border-gray-300 bg-gray-50 focus:ring-primary-300 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-primary-600"
+                        />
+                        <label htmlFor="checkbox-all" className="sr-only">
+                          checkbox
+                        </label>
+                      </div>
+                    </th>
+                    <th
+                      scope="col"
+                      className="p-4 text-left text-xs font-medium uppercase text-gray-500 dark:text-gray-400"
+                    >
+                      Name
+                    </th>
+
+                    <th
+                      scope="col"
+                      className="p-4 text-left text-xs font-medium uppercase text-gray-500 dark:text-gray-400"
+                    >
+                      gender
+                    </th>
+
+                    <th
+                      scope="col"
+                      className="p-4 text-left text-xs font-medium uppercase text-gray-500 dark:text-gray-400"
+                    >
+                      กลุ่มแคร์
+                    </th>
+                    <th
+                      scope="col"
+                      className="p-4 text-left text-xs font-medium uppercase text-gray-500 dark:text-gray-400"
+                    >
+                      job
+                    </th>
+                    <th
+                      scope="col"
+                      className="p-4 text-left text-xs font-medium uppercase text-gray-500 dark:text-gray-400"
+                    >
+                      interesting
+                    </th>
+                    <th
+                      scope="col"
+                      className="p-4 text-left text-xs font-medium uppercase text-gray-500 dark:text-gray-400"
+                    >
+                      persernality
+                    </th>
+                    {/* <!-- <th
+                  scope="col"
+                  class="p-4 text-left text-xs font-medium uppercase text-gray-500 dark:text-gray-400"
+                >
+                  Address
+                </th> --> */}
+                    <th
+                      scope="col"
+                      className="p-4 text-left text-xs font-medium uppercase text-gray-500 dark:text-gray-400"
+                    >
+                      status
+                    </th>
+                    <th
+                      scope="col"
+                      className="p-4 text-left text-xs font-medium uppercase text-gray-500 dark:text-gray-400"
+                    >
+                      Action
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="divide-y divide-gray-200 bg-white dark:divide-gray-700 dark:bg-gray-800">
+                  {lambs.map((user: Lamb) => (
+                    <tr
+                      key={user.id}
+                      className="hover:bg-gray-100 dark:hover:bg-gray-700"
+                    >
+                      <td className="w-4 p-4">
+                        <div className="flex items-center">
+                          <input
+                            id="checkbox-{{ .id }}"
+                            aria-describedby="checkbox-1"
+                            type="checkbox"
+                            className="focus:ring-3 h-4 w-4 rounded border-gray-300 bg-gray-50 focus:ring-primary-300 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-primary-600"
+                          />
+                          <label
+                            htmlFor="checkbox-{{ .id }}"
+                            className="sr-only"
+                          >
+                            checkbox
+                          </label>
+                        </div>
+                      </td>
+                      <td className="mr-12 flex items-center space-x-6 whitespace-nowrap p-4">
+                        <img
+                          className="h-24 w-24 rounded-full"
+                          src={
+                            user.profilePicture
+                              ? user.profilePicture
+                              : '/user.png'
+                          }
+                          alt={`${user.nickName} avatar`}
+                        />
+                        <div className="text-sm font-normal text-gray-500 dark:text-gray-400">
+                          <div className="text-base font-semibold text-gray-900 dark:text-white">
+                            {user.firstName} {user.lastName}
+                          </div>
+                          <div className="text-sm font-normal text-gray-500 dark:text-gray-400">
+                            {user.nickName}
+                          </div>
+                        </div>
+                      </td>
+                      <td className="max-w-sm overflow-hidden truncate p-4 text-base font-normal text-gray-900 dark:text-gray-400 xl:max-w-xs">
+                        <div
+                          className={`m-2 inline-block rounded-md p-2 ${user.gender == 'male' ? 'bg-sky-200' : 'bg-red-200'}`}
+                        >
+                          {user.gender}
+                        </div>
+                      </td>
+                      <td className="max-w-sm overflow-hidden truncate p-4 text-base font-normal text-gray-900 dark:text-gray-400 xl:max-w-xs">
+                        {user.groupCare}
+                      </td>
+                      <td className="max-w-sm overflow-hidden truncate p-4 text-base font-normal text-gray-900 dark:text-gray-400 xl:max-w-xs">
+                        {user.job}
+                      </td>
+                      <td className="max-w-sm overflow-hidden truncate p-4 text-base font-normal text-gray-900 dark:text-gray-400 xl:max-w-xs">
+                        {user.interesting}
+                      </td>
+                      <td className="max-w-sm overflow-hidden truncate p-4 text-base font-normal text-gray-900 dark:text-gray-400 xl:max-w-xs">
+                        <div
+                          className="m-4 inline-block rounded-md p-4"
+                          style={{ backgroundColor: getPersoanlityColor(user.persernality) }}
+                        >
+                          <strong>{user.persernality}</strong>
+                        </div>
+                      </td>
+
+                      {/* <td class="whitespace-nowrap p-4 text-base font-medium text-gray-900 dark:text-white">
+                        {user.address}
+                      </td> */}
+                      <td className="whitespace-nowrap p-4 text-base font-normal text-gray-900 dark:text-white">
+                        <div className="flex items-center">
+                          {user.status === 'Active' ? (
+                            <div className="mr-2 h-2.5 w-2.5 rounded-full bg-green-400" />
+                          ) : (
+                            <div className="mr-2 h-2.5 w-2.5 rounded-full bg-red-500" />
+                          )}
+                          {user.status}
+                        </div>
+                      </td>
+                      <td className="space-x-2 whitespace-nowrap p-4">
+                        <HeaderLink href={url(`settings/${user.id}`)}>
+                          <button
+                            type="button"
+                            className="inline-flex items-center rounded-lg bg-primary-700 px-3 py-2 text-center text-sm font-medium text-white hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                          >
+                            <svg
+                              className="mr-2 h-4 w-4"
+                              fill="currentColor"
+                              viewBox="0 0 20 20"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
+                              <path
+                                fillRule="evenodd"
+                                d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
+                                clipRule="evenodd"
+                              />
+                            </svg>
+                            Edit
+                          </button>
+                        </HeaderLink>
+
+                        <button
+                          type="button"
+                          data-modal-target="delete-user-modal"
+                          data-modal-toggle="delete-user-modal"
+                          className="inline-flex items-center rounded-lg bg-red-600 px-3 py-2 text-center text-sm font-medium text-white hover:bg-red-800 focus:ring-4 focus:ring-red-300 dark:focus:ring-red-900"
+                        >
+                          <svg
+                            className="mr-2 h-4 w-4"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                              clipRule="evenodd"
+                            />
+                          </svg>
+                          Delete user
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
